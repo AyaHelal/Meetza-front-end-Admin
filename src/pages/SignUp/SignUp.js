e0942996b133cffaf97de3c24a9c25f8cb2e757a
Farida Emad <far.emad888@gmail.com>
2026-01-02 21:14:22 +0200

    adjust login and dashboard responsive

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -7,7 +7,7 @@ export default function SignUp() {
         <div className="container-fluid">
             <div className="row py-2">
                 <motion.div
-                    className="col-md-6 d-none d-md-flex justify-content-center align-items-center"
+                    className="col-lg-6 d-none d-lg-flex justify-content-center align-items-center"
                     initial={{ opacity: 0, x: -100 }}
                     animate={{ opacity: 1, x: 0 }}
                     exit={{ opacity: 0, x: 100 }}
@@ -17,7 +17,7 @@ export default function SignUp() {
                 </motion.div>
 
                 <motion.div
-                    className="col-12 col-md-6 d-flex justify-content-center"
+                    className="col-12 col-lg-6 d-flex justify-content-center"
                     initial={{ opacity: 0, x: 100 }}
                     animate={{ opacity: 1, x: 0 }}
                     exit={{ opacity: 0, x: -100 }}
